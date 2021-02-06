e1d9dac63e98d14a677d982e4134ffe49cc39407
DanielaMoreiraTorres <66782858+DanielaMoreiraTorres@users.noreply.github.com>
2021-02-06 15:01:52 -0500

    update diseño

--- app/src/main/java/com/artesaniasclient/activity_contacts.java
+++ app/src/main/java/com/artesaniasclient/activity_contacts.java
@@ -1,14 +1,53 @@
 package com.artesaniasclient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class activity_contacts extends AppCompatActivity {
+
+    RecyclerView rcvContacts;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        rcvContacts = findViewById(R.id.rcvContacts);
+        LinearLayoutManager linear = new LinearLayoutManager(getApplicationContext());
+        linear.setOrientation(LinearLayoutManager.VERTICAL);
+        rcvContacts.setLayoutManager(linear);
+        rcvContacts.hasFixedSize();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.btnLogIn) {
+
+        }
+        if(id == R.id.btnContacts) {
+            Intent intent = new Intent(this, activity_contacts.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
