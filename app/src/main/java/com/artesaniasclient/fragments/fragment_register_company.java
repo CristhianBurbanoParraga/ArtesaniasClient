73bd30f18ca76b4c541a5b6b6465edf62109db5e
DanielaMoreiraTorres <66782858+DanielaMoreiraTorres@users.noreply.github.com>
2021-02-28 01:16:33 -0500

    cambiar de activity a fragment_register_company

--- app/src/main/java/com/artesaniasclient/fragments/fragment_register_company.java
+++ app/src/main/java/com/artesaniasclient/fragments/fragment_register_company.java
@@ -1,5 +1,6 @@
 package com.artesaniasclient.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.artesaniasclient.R;
+import com.artesaniasclient.activity_empresa;
+import com.artesaniasclient.activity_principal;
+import com.artesaniasclient.controller.CompanyController;
+import com.artesaniasclient.interfaces.ICompanyComunication;
+import com.artesaniasclient.model.Company;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link fragment_register_company#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_register_company extends Fragment {
+public class fragment_register_company extends Fragment implements ICompanyComunication {
+
+    private CompanyController companyController;
+
+    private EditText txtNameBussines;
+    private EditText txtRuc;
+    private EditText txtCity;
+    private EditText txtAddress;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +78,51 @@ public class fragment_register_company extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_company, container, false);
+        View view = inflater.inflate(R.layout.fragment_crafts, container, false);
+        companyController = new CompanyController(this);
+
+        txtNameBussines = view.findViewById(R.id.namebussines);
+        txtRuc = view.findViewById(R.id.ruc);
+        txtCity = view.findViewById(R.id.city);
+        txtAddress = view.findViewById(R.id.address);
+        return view;
+    }
+
+    public void registry_company(View view) {
+        Company company = new Company();
+        company.setBusinessname(txtNameBussines.getText().toString());
+        company.setRuc(txtRuc.getText().toString());
+        company.setCity(txtCity.getText().toString());
+        company.setAddress(txtAddress.getText().toString());
+        company.setDateregistry(new Date().toString());
+        company.setIsactive(false);
+        companyController.addCompany(company);
+    }
+
+    public void cancel_company(View view) {
+        Intent intent = new Intent(getContext(), activity_principal.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void add_company_success(Company c, String message) {
+        if (c == null && c.getId() == null) {
+            Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        } else {
+            message = "Empresa registrada exitosamente";
+            Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getContext(), activity_principal.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void get_company_success(ArrayList<Company> companies, String message) {
+
+    }
+
+    @Override
+    public void delete_company_success(Company c, String message) {
+
     }
 }
